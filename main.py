3b32472cf706739e3fb417fb087226a838b7b5a6
Technology Rooms <technologyrooms@colsanjose.edu.co>
2025-01-23 08:36:43 -0500

    replace

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape(url):
+tags = [
+    "<p>",
+    "</p>",
+    "</div>",
+    "<strong>",
+    "</strong>",
+    "<div class=\"mcontent\">",
+    "<b>",
+    "<ul>",
+    "</b>",
+    "<ul class=\"qlist\">",
+    "<li>",
+    "</li>",
+    "</ul>",
+    "<code>",
+    "</code>"
+    
+]
 
-    response = requests.get(url)
-    response.raise_for_status()
-    soup=BeautifulSoup(response.text,'html.parser')
-    question_text = soup.select_one("div.content").get_text(strip=True)
-    return question_text
+def scrape():
+    for i in range(1, 2):
+        url = "https://www.crackap.com/ap/computer-science-a/question-"+ str(i) + "-answer-and-explanation.html"
+        response = requests.get(url)
+        response.raise_for_status()
+        soup=BeautifulSoup(response.text,'html.parser')
+        content = soup.find("div", class_="mcontent")
+        contentList = list(content.children)
+        content = str(content)
+        for i in tags:
+            content = content.replace(i, "")
+       # return str(content)
+        return content
 
 
 
-data = scrape("https://www.crackap.com/ap/computer-science-a/question-1-answer-and-explanation.html")
+data = scrape()
 print(data)
 
 
